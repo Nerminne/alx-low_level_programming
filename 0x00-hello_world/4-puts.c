d30caebce7d0760d4317fc74f21e6ea838f5db85
nerminne <nerminee.mostafaa@gmail.com>
2023-08-10 18:59:14 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point where execution takes place
+ *
+ * return: Always 0 means program implemeentation successfully
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
