b6380a381d087717c92f332488686329c71420ce
nerminne <nerminee.mostafaa@gmail.com>
2023-08-10 17:10:11 -0700

    preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point where the execution process takes place
+ *
+ * Return: Always 0, which means the program implemented successfully
+ * 
+ * void: means no output will return
+ */
+int main(void)
+{
+	return (0);
+}
