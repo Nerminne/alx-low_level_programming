79429ac92f4871394734b2489d1c656fc6bd901d
nerminne <nerminee.mostafaa@gmail.com>
2023-08-16 15:37:21 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,20 +26,22 @@ void print_times_table(int n)
 			{
 				_putchar((prod / 100) + '0');
 				_putchar(((prod / 10) % 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 
-			else if (prod <= 9)
+			else if (prod > 9 && prod < 100)
 			{
 				_putchar(' ');
-				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 			else
 			{
 				_putchar(' ');
-				_putchar((prod / 10) + '0');
+				_putchar(' ');
+				_putchar((prod % 10) + '0');
 			
 			}
-			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
