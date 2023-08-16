1b4ab6757e5aa7015aa03c28155401b80bd29819
nerminne <nerminee.mostafaa@gmail.com>
2023-08-15 19:20:45 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - return sum of two num
+ * Return: Always 0 (Success)
+ * @x: num1
+ * @y: num2
+ * main.h is header file contain prototype of functions used
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
