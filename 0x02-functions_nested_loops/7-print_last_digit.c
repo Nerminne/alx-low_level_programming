fbc0a0cfc1aa563023602fafd9ae26d289b41617
nerminne <nerminee.mostafaa@gmail.com>
2023-08-15 17:35:35 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @n: number to get its last digit
+ * Return: Always 0 (Success)
+ *
+ * main.h is header file contain prototype of functions used
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = -n;
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
