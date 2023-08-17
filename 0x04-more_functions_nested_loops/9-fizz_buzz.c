60b40f68757ee38fb1bde3768a52a8601410ac92
nerminne <nerminee.mostafaa@gmail.com>
2023-08-17 11:21:07 -0700

    fix 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0)
-			printf("Fizz ");
-		else if ((n % 5) == 0 && (n != 100))
-			printf("Buzz ");
-		else if (n == 100)
+			printf("Fizz");
+		else if ((n % 5) == 0)
 			printf("Buzz");
 		else if ((n % 3) == 0 && (n % 5) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
 			printf("%d ", n);
+		if (n < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
