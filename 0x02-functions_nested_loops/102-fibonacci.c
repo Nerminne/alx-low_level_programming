0cdb3a2bb3c52bbee1cec92788fe34c7399c6b6a
nerminne <nerminee.mostafaa@gmail.com>
2023-08-16 17:27:19 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : prints first 50 fibonacci numbers
+ *
+ * Return: Always (0) (Success)
+*/
+
+int main(void)
+{
+	int f;
+	int f1 = -1;
+	int f2 = 1;
+	int counter;
+
+	for (counter = 0; counter < 50; counter++)
+	{
+		f = f1 + f2;
+		printf("%d, ", f);
+		f1 = f2;
+		f2 = f;
+	}
+	return (0);
+}
