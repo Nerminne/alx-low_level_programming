fc034c1c921004b8b6784739edf225fb34658b67
nerminne <nerminee.mostafaa@gmail.com>
2023-08-21 18:01:49 -0700

    string rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to be printed.
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int lenght = 0;
+	char rev;
+
+	while (s[i++] != '\0')
+		lenght++;
+	for (i = lenght - 1; i >= lenght / 2; i--)
+	{
+		rev = s[i];
+		s[i] = s[lenght - i -1];
+		s[lenght - i - 1] = rev;
+	}
+}
