e74752860bd1344a8ff515dc9ae0ab1d8ce66c08
nerminne <nerminee.mostafaa@gmail.com>
2023-08-21 18:26:17 -0700

    last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string, followed by a new line
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i = 0, lenght = 0;
+
+	while (str[i++])
+		lenght++;
+	if (lenght % 2 != 0)
+		lenght = (lenght - 1) / 2;
+	else
+		lenght /= 2;
+	for (i = lenght; i <= lenght * 2; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
