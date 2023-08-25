072a48169d607cd9a936ef5a9c77c3dba236e8c3
nerminne <nerminee.mostafaa@gmail.com>
2023-08-24 19:47:40 -0700

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* rot13 - encoder string using rot13
+* @s: pointer to string params
+*
+* Return: converted string
+*/
+char *rot13(char *s)
+{
+	int i, x;
+	char ch1[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtYyUuVvWwXxYyZz";
+	char rot[] = "NnOoPpQqRrSsTtYyUuVvWwXxYyZAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; ch1[x] != '\0'; x++)
+		{
+			if (s[i] == ch1[x])
+				s[i] = rot[x];
+		}
+	}
+	return (s);
+}
