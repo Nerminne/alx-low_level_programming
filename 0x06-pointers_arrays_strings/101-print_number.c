69ae5ce6fb5a6b06d125cef3f922aa6bd3f3599d
nerminne <nerminee.mostafaa@gmail.com>
2023-08-25 17:51:23 -0700

    int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,26 +6,16 @@
  */
 void print_number(int n)
 {
-	int i;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	for (i = 10; ; i *= 10)
-	{
-		if (n < i)
-			break;
-	}
-	i /= 10;
-	while (i > 10)
-	{
-		_putchar((n / i) + '0');
-		n = n - ((n / i) * i);
-		i /= 10;
-	}
-		_putchar((n / i) + '0');
-	if (n >= 10)
-		_putchar((n % i) + '0');
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
