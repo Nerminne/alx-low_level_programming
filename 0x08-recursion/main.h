dcfde0725de813dbcb2305f136b1f2b49ef0e0a9
nerminne <nerminee.mostafaa@gmail.com>
2023-08-30 00:30:52 -0700

    puts

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file main.h contain prototype of
+ * all function that will used in this directory
+ *
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
