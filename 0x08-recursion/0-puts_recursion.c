4dd200bec6eb8db37452f8061c68bfdc4ccaa9ca
nerminne <nerminee.mostafaa@gmail.com>
2023-08-30 01:11:05 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s++);
-	if (*s != '\0' || *(s + 0) == '\0')
-		_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
 	else
 		_putchar('\n');
 }
