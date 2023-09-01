8b9db1d99c3b760a07d4ef9b47e8c5e9365d0196
nerminne <nerminee.mostafaa@gmail.com>
2023-09-01 02:31:30 -0700

    one shot

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * Description: program prints its name followed by newline
  * @argc: argument counter
- * @argv: argument vector, pointer to array of arguments
+ * @argv: argument vector, pointer to array of argument
  * Return: Always (0) (Success)
 */
 int main(int __attribute__((__unused__)) argc, char **argv)
