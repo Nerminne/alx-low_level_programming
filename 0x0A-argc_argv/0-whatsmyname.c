2062489877dfd6a09f27a446fc19c794d8bb23f7
nerminne <nerminee.mostafaa@gmail.com>
2023-09-01 03:14:49 -0700

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: arguments vector
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
