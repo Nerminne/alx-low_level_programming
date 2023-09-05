2477e347b1d09adcaa4ae7dd0b092d44ee27ed68
nerminne <nerminee.mostafaa@gmail.com>
2023-09-05 01:27:42 -0700

    duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * create_array - creates an array of chars,
- * and initializes it with a specific char.
- * @size: size of an array
- * @c: character to be initialized
- * Return: specific char
+ * _strdup -  returns a pointer to a new string
+ * which is a duplicate of the string str.
+ * @str: array to be duplicated
+ * Return: pointer to string which is duplicate of str
  */
+char *_strdup(char *str)
+{
+	char *dupl_str;
+	unsigned long int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	dupl_str = malloc(sizeof(char) * strlen(str));
+	if (dupl_str == NULL)
+		return (NULL);
+	while (i < strlen(str))
+	{
+		dupl_str[i] = str[i];
+		i++;
+	}
+	return (dupl_str);
+}
