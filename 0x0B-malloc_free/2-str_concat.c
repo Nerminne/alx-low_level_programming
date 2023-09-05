664e6e319f9da573262041ef113cf9adcf2083a9
nerminne <nerminee.mostafaa@gmail.com>
2023-09-05 03:05:07 -0700

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,24 +10,36 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
-	unsigned long int x = 0, i = 0;
+	unsigned long int x = 0, i = 0, len1, len2;
+
 	if (strlen(s1) == 0)
+	{
 		s1 = "";
+		len1 = 0;
+	}
+	else
+		len1 = strlen(s1);
 	if (strlen(s2) == 0)
+	{
 		s2 = "";
-	concat_str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+		len2 = 0;
+	}
+	else
+		len2 = strlen(s2);
+	concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat_str == NULL)
 		return (NULL);
-	while (strlen(s1) > i)
+	while (s1[i])
 	{
 		concat_str[i] = s1[i];
 		i++;
 	}
-	while (strlen(s2) >= x)
+	while (s2[x])
 	{
 		concat_str[i] = s2[x];
 		x++;
 		i++;
 	}
+	concat_str[i] = '\0';
 	return (concat_str);
 }
