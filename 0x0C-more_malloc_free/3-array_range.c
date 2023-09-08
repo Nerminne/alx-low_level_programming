e611bb8a9494e09e50632e836e22d97200dde622
nerminne <nerminee.mostafaa@gmail.com>
2023-09-07 17:31:26 -0700

    array-range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	array = malloc(sizeof(int) * len);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; min < len; i++)
+	for (i = 0; i < len; i++)
 		array[i++] = min++;
 	return (array);
 }
