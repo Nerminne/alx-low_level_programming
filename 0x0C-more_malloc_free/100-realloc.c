f7399324dde2bbb72bb234f5d049d03c28e280f9
nerminne <nerminee.mostafaa@gmail.com>
2023-09-07 18:53:45 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,12 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-	{	
-       		new_ptr = malloc(sizeof(char) * (new_size));
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
-	}
+		old_size = 0;
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
