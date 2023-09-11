d5df379d9caa515a340f84e2873562a7152cfe67
nerminne <nerminee.mostafaa@gmail.com>
2023-09-11 14:26:53 -0700

    new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * file dog.h contain definition of new type struct for dogs 
+ * and prototype of all function that
+ * will used in this directory
+ */
+
+/**
+ * struct dog - structure for dogs
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: structure that contain info about dogs
+ * name, age and their owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
