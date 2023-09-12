3b9c37d004982b9b80aecf91d0c7b2871e1f1a4c
nerminne <nerminee.mostafaa@gmail.com>
2023-09-11 19:23:54 -0700

    filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,10 @@
 /**
  * main - program that prints the name
  * of the file it was compiled from followed by a new line
- * Return: nothing
+ * Return: Always 0 Success
  */
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
