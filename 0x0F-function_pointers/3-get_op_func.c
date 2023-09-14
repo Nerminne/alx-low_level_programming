1e83c284ac3332fd8e1d01ef90102997e0e00dd4
nerminne <nerminee.mostafaa@gmail.com>
2023-09-13 19:56:58 -0700

    operator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,28 @@
 #include "3-calc.h"
 /**
- * get_op_func - function perform the operation asked by the user
- * @s: operator passed as argument to the program
- * Return: pointer to the function
- * that corresponds to the operator given
- */
-int (*get_op_func(char *s))(int a, int b)
+ * get_op_func - a func that gets operator function
+ * @s: operator
+ * Return: result
+*/
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-    
-    while (*op)
-    {
-	    if (*(ops + i).op[0] == *s)
-		return (ops[i].f);
-	    i++;
-    }
-    return (NULL);
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,40 +1,40 @@
 #include "3-calc.h"
 /**
- * op_add - function that add two int
- * @a: first int
- * @b: second int
- * Return: sum of a and b
- */
+ * op_add - adds
+ * @a: int
+ * @b: int
+ * Return: result
+*/
 int op_add(int a, int b)
 {
-	return (a + b)
+	return (a + b);
 }
-/**  
- * op_add - function that subtract two int
- * @a: first int
- * @b: second int
- * Return: result of subtraction of a and b
- */
+/**
+ * op_sub - subs
+ * @a: int
+ * @b: int
+ * Return: result
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
-  *op_mul - returns product of two numbers.
-  *@a: first int
-  *@b: second int
-  *Return: product of a and b
-  */
+ * op_mul - adds
+ * @a: int
+ * @b: int
+ * Return: result
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
-  *op_div - returns the division of two numbers.
-  *@a: first int
-  *@b: second int
-  *Return: a divided by b
-  */
+ * op_div - adds
+ * @a: int
+ * @b: int
+ * Return: result
+*/
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -45,11 +45,11 @@ int op_div(int a, int b)
 	return (a / b);
 }
 /**
-  *op_mod - returns the remainder of division.
-  *@a: first int
-  *@b: second int
-  *Return: remainder of division
-  */
+ * op_mod - adds
+ * @a: int
+ * @b: int
+ * Return: result
+*/
 int op_mod(int a, int b)
 {
 	if (b == 0)
