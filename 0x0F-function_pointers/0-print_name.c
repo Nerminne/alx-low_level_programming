c74d246aabc68b82e0089cb1548d360ee2effe0f
nerminne <nerminee.mostafaa@gmail.com>
2023-09-13 17:25:53 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function pointer that require string as argument
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * file function_pointers.h contain prototype of
+ * all function that will used in this directory
+ */
+void print_name(char *name, void (*f)(char *));
+
+#endif
