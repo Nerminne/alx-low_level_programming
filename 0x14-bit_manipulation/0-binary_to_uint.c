f024d5552e2d18b544a11b4e7b83568c7c908f45
Nerminne <nerminee.mostafaa@gmail.com>
2023-09-28 15:05:18 -0700

    unsigned int to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,9 +7,10 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int weight = 1;
+	unsigned int weight = 1;
 	int i = 0;
 	unsigned int decimal = 0;
+
 	if (b == NULL)
 		return (0);
 	i = (int)strlen(b) - 1;
