916978a9238f133a5aa9f152fe6217b7301375e8
Nerminne <nerminee.mostafaa@gmail.com>
2023-11-09 18:38:56 -0800

    add node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		idx--;
 	}
-	if (temp->next == NULL || idx == -1)
+	if (temp->next == NULL || idx == 1)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
