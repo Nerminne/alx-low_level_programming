fde3a8bf1113d0d70eb431c524f3eec58ec84501
Nerminne <nerminee.mostafaa@gmail.com>
2023-12-30 11:47:56 -0800

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of hash table
+ * Return: pointer to a hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+	hash_node_t **hash_arr;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	hash = malloc(sizeof(size));
+	if (hash == NULL)
+		return (NULL);
+	hash_arr = malloc(sizeof(*hash_arr) * size);
+	if (hash_arr == NULL)
+	{
+		free(hash);
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+			hash_arr[i] = NULL;
+	}
+	hash->size = size;
+	hash->array = hash_arr;
+
+	return (hash);
+}
